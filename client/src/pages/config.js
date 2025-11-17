9ecb7df850fd0ff4d366cb774b8ad90d2210b1b2
swathichaganti91 <suseelalucky227@gmail.com>
2025-11-17 19:57:43 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://ab6f0a18b467d4b0397c62db9c0a0e38-491344043.us-east-1.elb.amazonaws.com";
+const API_BASE_URL = "http://15.206.186.180:84";
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
